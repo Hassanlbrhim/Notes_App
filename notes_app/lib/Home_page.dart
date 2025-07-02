a7142c21500458692d461fde7a930698fab66add
hassan_badran <you@example.com>
2025-07-02 10:50:47 +0300

    NotesApp

--- notes_app/lib/Home_page.dart
+++ notes_app/lib/Home_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
-class  NotesApp extends StatelessWidget {
+class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Notes App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Notes App'),
+        ),
+        body: const Center(
+          child: Text('Welcome to Notes App!'),
+        ),
+      ),
+    );
   }
 }
