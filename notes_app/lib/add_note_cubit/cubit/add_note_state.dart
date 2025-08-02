17ef45540c167901dda685a69a25449401de0fd5
hassan_badran <you@example.com>
2025-08-02 22:52:54 +0300

    add not state

--- notes_app/lib/add_note_cubit/cubit/add_note_state.dart
+++ notes_app/lib/add_note_cubit/cubit/add_note_state.dart
@@ -4,3 +4,13 @@ part of 'add_note_cubit.dart';
 abstract class AddNoteState {}
 
 class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoding extends AddNoteState {}
+
+class AddNoteSucess extends AddNoteState {}
+
+class AddNoteFailuer extends AddNoteState {
+  final String errMessage;
+
+  AddNoteFailuer({required this.errMessage});
+}
